dd1721aec5883fefe477c5d048ccf829a42d007b
Makwana Dhaval <18bce108@nirmauni.ac.in>
2021-08-27 14:38:56 +0530

    check for empty string

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -4,5 +4,10 @@ public class SimpleCalculator {
 	public SimpleCalculator() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public int add(String str)
+	{
+		return 0;
+	}
+	
 }
--- src/SimpleCalculatorTest.java
+++ src/SimpleCalculatorTest.java
@@ -1,5 +1,14 @@
 import junit.framework.TestCase;
 
-public class SimpleCalculatorTest extends TestCase {
-
+public class SimpleCalculatorTest extends TestCase 
+{
+	SimpleCalculator simpleCalculator;
+	public void setUp()
+	{
+		simpleCalculator = new SimpleCalculator();
+	}
+	public void testEmptyString()
+	{
+		assertTrue(simpleCalculator.add("") == 0);
+	}
 }
